1c76c60b9c52861e6298e2a8cb875444131ba996
Jeff Stock <jeffstock@MacBook-Air-6.local>
2019-09-26 18:09:46 -0700

    plain text email working

---
+++ emailer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import smtplib, ssl, getpass
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "stocksandbox@gmail.com"  # Enter your address
+receiver_email = "jantonstock@gmail.com"  # Enter receiver address
+password = getpass.getpass()
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
