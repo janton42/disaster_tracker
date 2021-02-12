12b0953fbbdc365d63ffda7b688fbde164dcffac
Jeff Stock <jeffstock@SCFVFC81WWLYWQ.local>
2021-02-11 22:29:05 -0800

    refactored

--- scraper.py
+++ scraper.py
@@ -5,55 +5,39 @@ import lxml
 import requests
 import re
 
+mainUrl = 'http://www.gdacs.org/XML/RSS.xml'
 resources = []
-def scrape():
 
-	
-	disasters = {}
-	counter = 0
-
-
-	url = 'http://www.gdacs.org/XML/RSS.xml'
+def makeSoup(url):
 	resp = requests.get(url)
 	soup = BeautifulSoup(resp.content, features="xml")
-	items = soup.findAll('item')
+	
+	return soup
 
-
+def scrape():
+	disasters = {}
+	counter = 0
+	items = makeSoup(mainUrl).findAll('item')
 	for i in items:
 		alert_level = i.find('alertlevel').text
-
 		if alert_level == 'Red':
 			resources.append(i.resources.findAll('resource'))
-
 	impact_xmls = []
 	impact_data = []
-
 	for r in resources:
 	  for i in r:
 	    if i['id'] == 'impact_xml':
 	      impact_xmls.append(i['url'])
 	    elif i['id'] == 'impact_data':
 	      impact_data.append(i['url'])
-
 	xml_calculations = []
 	xml_contentdata = []
-
-
-
 	for xml in impact_xmls:
 	  calc_match = re.search('calculation', xml)
-	  # data_match = re.search('contentdata', xml)
 	  if calc_match:
 	    xml_calculations.append(xml)
-	  
-	  # if data_match:
-	  #   xml_contentdata.append(xml)
-
-
 	for site in xml_calculations:
-	  resp_3 = requests.get(site)
-	  soup_3 = BeautifulSoup(resp_3.content, features="xml")
-	  datums = soup_3.findAll('datums')
+	  datums = makeSoup(site).findAll('datums')
 	  for d in datums:
 	    if d['alias'] == 'City':
 	      data = d.findAll('datum')
@@ -64,13 +48,11 @@ def scrape():
 	          if name.text == 'NAME':
 	            disaster = {}
 	            counter += 1
-	            disaster['city'] = scalar.value.text
-	          
+	            disaster['city'] = scalar.value.text      
 	          if name.text == 'COUNTRY':
 	            disaster['country'] = scalar.value.text
 	            disaster['type'] = soup_3.find('model-name').text
 	            disasters[counter] = disaster
-
 	for link in impact_data:
 	  resp_4 = requests.get(link)
 	  soup_4 = BeautifulSoup(resp_4.content, 'lxml')
